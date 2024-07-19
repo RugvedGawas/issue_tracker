e049ecedf56b13fdb15b399898245fd0863a8b58
Rugved Gawas <rugvedgawas0018@gmail.com>
2024-07-19 11:43:12 +0530

    additional loading skeleton

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,9 +1,25 @@
+import IssueStatusBadge from '@/app/components/IssueStatusBadge'
+import { Box, Card, Flex, Text } from '@radix-ui/themes'
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const loadingIssueDetailPage = () => {
   return (
   <>
-  <h1>Loading...</h1>
+ <Box>
+<Skeleton className='max-w-xl'/>
+  <Flex gap='2' my='2'>
+  <Skeleton width='5rem' />
+  <Skeleton width='8rem'/>
+
+  </Flex>
+  <Card className='prose' mt='4'>
+  <Skeleton count={3}/>
+  </Card>
+
+</Box>
   </>
   )
 }
